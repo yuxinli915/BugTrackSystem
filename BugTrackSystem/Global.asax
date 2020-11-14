7561354881517988b9108b122a06c27e9d30d93c
Yuxin Li <yuxinli915@outlook.com>
2020-11-13 20:45:30 -0600

    Add project files.

---
+++ BugTrackSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BugTrackSystem.MvcApplication" Language="C#" %>
---
+++ BugTrackSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugTrackSystem.Startup))]
+namespace BugTrackSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
