d26fb90c58a6f7c689b633796b75fcb928538391
Kevin Luong <luongk88@gmail.com>
2020-11-16 15:02:38 -0600

    created userhelper methods for notifications and role management. Missing create user and edit user detail

---
+++ BugTrackSystem/Models/UserHelper.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackSystem.Models
+{
+    public class UserHelper
+    {
+        static ApplicationDbContext db = new ApplicationDbContext();
+        static UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>
+            (new UserStore<ApplicationUser>(db));
+        static RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>
+            (new RoleStore<IdentityRole>(db));
+        
+        public static void CreateRole(string roleName)
+        {
+            if (!roleManager.RoleExists(roleName))
+                roleManager.Create(new IdentityRole { Name = roleName });
+        }
+
+        public static void DeleteRole(string roleName)
+        {
+            if (roleManager.RoleExists(roleName))
+            {
+                var role = roleManager.FindByName(roleName);
+                roleManager.Delete(role);
+            }
+        }
+
+        public static void RemoveUserFromRole(string userId, string roleName)
+        {
+            var user = userManager.FindById(userId);
+            var currentUserRole = user.Roles.ToString();
+
+            if (!roleManager.RoleExists(roleName))
+                roleManager.Create(new IdentityRole { Name = roleName });
+
+            userManager.RemoveFromRole(userId, currentUserRole);
+        }
+
+        public static void AddUserToRole(string userId, string roleName)
+        {
+            if (!userManager.IsInRole(userId, roleName))
+                userManager.AddToRole(userId, roleName);
+        }
+
+        public static void CreateAssignedNotification(string userId)
+        {
+            Developer user = (Developer)userManager.FindById(userId);
+            var assignedNotification = new TicketNotificaiton();
+            assignedNotification.User = user;
+            assignedNotification.UserId = userId;
+            assignedNotification.Body = $"Created: {DateTime.Now.ToString()} - {user.UserName} has been assigned a new ticket.";
+        }
+
+        public static void CreateTicketChangeNotification(string userId, int ticketId)
+        {
+            Developer user = (Developer)userManager.FindById(userId);
+            var ticketChangeNotification = new TicketNotificaiton();
+            ticketChangeNotification.User = user;
+            ticketChangeNotification.UserId = userId;
+            ticketChangeNotification.Body = $"Created: {DateTime.Now.ToString()} - there was a change to ticket #{ticketId}.";
+        }
+    }
+}
